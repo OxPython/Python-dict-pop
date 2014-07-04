4b0f5a8d3915603a14621fd2f2907b99bd5c2918
Emerson <root@emerson-N53SN>
2014-07-04 05:40:22 -0500

    HowTo delete a key and keeping the value at the same time in python?

--- dict_pop.py
+++ dict_pop.py
@@ -1,25 +1,23 @@
 '''
-Created on Jul 1, 2014
+Created on Jul 2, 2014
 
 @author: viejoemer
+
+HowTo delete a key and keeping the value at the same time in python?
+
+¿Cómo eliminar una clave y mantener el valor, al mismo tiempo en python?
+
 '''
-############################################################
-#HowTo delete a key and keeping the value at the same time in python?
 
 #Creating a dict with data
-
 d = {
      "red":100,
      "yellow":200,
      "blue" : 300
      }
-
 print(d)
 
 #Using pop to delete a key and keeping the value
 value = d.pop("red")
-
 print(d)
 print(value)
-
-print(d)
